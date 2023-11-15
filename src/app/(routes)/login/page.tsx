8865f045eb5c8bb54bfb5a6a46344a7ace777394
Scherbakov Ilya <scherbakov720@gmail.com>
2023-11-15 14:23:41 +0300

    Add filtering to the home page
    Добавлена возможность фильтровать тесты по названию на домашней странице

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { EntryForm } from '@/src/components';
 
 function Login(): React.ReactNode {
-  return (
-    <EntryForm
-      redirectTo="/register"
-      title="Войдите в аккаунт"
-      submitTitle="Войти"
-      redirectTitle="Нет учетной записи?"
-    />
-  );
+	return (
+		<EntryForm
+			redirectTo="/register"
+			title="Войдите в аккаунт"
+			submitTitle="Войти"
+			redirectTitle="Нет учетной записи?"
+		/>
+	);
+
 }
 
 export default Login;
