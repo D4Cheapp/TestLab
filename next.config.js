bca3aa3437d6a99c274c0cbadcf96f345d42d5c3
Scherbakov Ilya <scherbakov720@gmail.com>
2023-12-28 22:54:10 +0300

    Refactor deploy info

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: 'build',
   sassOptions: {
     prependData: `@import "src/app/_variables.scss";`
   },
